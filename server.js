37cb87ae3db7c21ab4dd0f8692d21485ec647b3b
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-03-15 14:04:49 -0700

    Added get requests to get the html and json files

--- server.js
+++ server.js
@@ -11,4 +11,27 @@
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  
+  // GET /notes should return the notes.html file
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'miniature-eureka/develop/public/notes.html'));
+  });
+
+  // GET / should return the index.html file
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'miniature-eureka/develop/public/index.html'));
+  });
+
+  // GET /api/notes should read the db.json file and return all saved notes as JSON
+  app.get("/api/notes", (req, res) => {
+      fs.readFile(path.join(__dirname, "miniature-eureka/develop/db/db.json"), "utf8", (err, data) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Failed to read notes from file." });
+        }
+    
+        res.json(JSON.parse(data));
+      });
+    });
+    
+
+ 
