f6b47e3a9b724feac0db3bf82739a50824e4792f
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-03-17 14:21:13 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,37 +1,88 @@
-  const express = require('express');
-  const fs = require('fs');
-  const path = require('path');
-  const { v4: uuidv4 } = require('uuid');
+// Import required modules
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
-  const app = express();
-  const PORT = process.env.PORT || 3001;
+// Initialize Express app
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-  // Middleware for parsing JSON and urlencoded form data
-  app.use(express.static('miniature-eureka/develop/public'));
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+// Middleware for serving static files, parsing JSON, and parsing urlencoded form data
+app.use(express.static('miniature-eureka/develop/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-  // GET /notes should return the notes.html file
-  app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'miniature-eureka/develop/public/notes.html'));
+// Route to serve notes.html when '/notes' is requested
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'miniature-eureka/develop/public/notes.html'));
+});
+
+// Route to serve index.html when the root path '/' is requested
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'miniature-eureka/develop/public/index.html'));
+});
+
+// Route to get all saved notes from db.json and return them as JSON
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "miniature-eureka/develop/db/db.json"), "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to read notes from file." });
+    }
+
+    res.json(JSON.parse(data)); 
   });
+});
 
-  // GET / should return the index.html file
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'miniature-eureka/develop/public/index.html'));
-  });
+// Route to create a new note and save it to db.json
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
 
-  // GET /api/notes should read the db.json file and return all saved notes as JSON
-  app.get("/api/notes", (req, res) => {
-      fs.readFile(path.join(__dirname, "miniature-eureka/develop/db/db.json"), "utf8", (err, data) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ error: "Failed to read notes from file." });
-        }
-    
-        res.json(JSON.parse(data));
-      });
+  fs.readFile(path.join(__dirname, 'miniature-eureka/develop/db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).end();
+    }
+
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile(path.join(__dirname, 'miniature-eureka/develop/db/db.json'), JSON.stringify(notes, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).end();
+      }
+      res.json(newNote);
     });
-    
+  });
+});
 
- 
+// Route to delete a note with the specified id from db.json
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile(path.join(__dirname, 'miniature-eureka/develop/db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).end();
+    }
+
+    let notes = JSON.parse(data);
+    notes = notes.filter((note) => note.id !== noteId);
+
+    fs.writeFile(path.join(__dirname, 'miniature-eureka/develop/db/db.json'), JSON.stringify(notes, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).end();
+      }
+      res.status(200).end();
+    });
+  });
+});
+
+// Start the Express server on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
+});
